26f93656f28d470b82ff42a4c78483b0cc67ed63
Soonrang <clearsbyul30@naver.com>
2023-08-18 01:42:19 +0900

    0818

--- src/main/java/org/zerock/b01/controller/SampleController.java
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -1,2 +1,26 @@
-package org.zerock.b01.controller;public class SampleController {
+package org.zerock.b01.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@Log4j2
+public class SampleController {
+    @GetMapping("/hello")
+    public void hello(Model model) {
+        log.info("안녕임다!");
+        model.addAttribute("msg","안녕 스프링부트~");
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+        List<String> list = Arrays.asList("AAA","BBB","CCC");
+        model.addAttribute("msg","안녕 스프링부트~");
+        model.addAttribute("list", list);
+    }
 }
