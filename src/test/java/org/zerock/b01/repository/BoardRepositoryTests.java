3a25d1916bddb6211338e761c504188bf65629f6
Soonrang <clearsbyul30@naver.com>
2023-08-22 13:20:00 +0900

    comm

--- src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.zerock.b01.domain.Board;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Log4j2
@@ -14,6 +17,20 @@ public class BoardRepositoryTests {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Test
+    public void testInsert() {
+        IntStream.rangeClosed(1,100).forEach(i -> {
+            Board board = Board.builder()
+                    .title("title..." +i)
+                    .content("content..." + i)
+                    .writer("user"+ (i % 10))
+                    .build();
+
+            Board result = boardRepository.save(board);
+            log.info("BNO: " + result.getBno());
+        });
+    }
 
     @Test
     public void testSearch1() {
